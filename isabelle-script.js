53791ebb3edee3f9eee8968f75bf42922978793e
Isabelle Franzen <isabelle.amsalu@gmail.com>
2025-09-29 10:37:55 +0200

    add event listener to close menu on page click

--- isabelle-script.js
+++ isabelle-script.js
@@ -1,12 +1,22 @@
 const burger = document.getElementById("burger");
 const menu = document.getElementById("list");
 
+/* Toggle menu */
 burger.addEventListener("click", () => {
   menu.classList.toggle("open");
+  menu.style.backgroundColor = "red";
 });
 
+/* Close menu on escape */
 document.body.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
+    menu.classList.remove("open");
+  }
+});
+
+/* Close menu on click outside menu */
+window.addEventListener("click", (event) => {
+  if (!menu.contains(event.target) && !burger.contains(event.target)) {
     menu.classList.remove("open");
   }
 });
