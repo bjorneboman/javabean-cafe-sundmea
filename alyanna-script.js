1068ff5bd20c2c2a46f60b4864e35e3ff048326e
Alyanna Karlsson <aok.karlsson@gmail.com>
2025-09-29 20:03:06 +0200

    placed menu on right side, changed color of font

--- alyanna-script.js
+++ alyanna-script.js
@@ -3,5 +3,5 @@ const burger = document.getElementById("burger");
 const list = document.getElementById("list");
 
 burger.addEventListener('click', () => {
-  list.classList.toggle('active');
+  list.classList.toggle('open');
 });
