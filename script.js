b2c70bc70fe6799ad92a68047237b63c59e6a74d
eliasjohansson123 <eliasjohansson9@gmail.com>
2025-09-29 11:21:42 +0200

    Responsiv CSS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const el = document.querySelector("#burger");
-const list = document.querySelector("#list");
+// const el = document.querySelector("#burger");
+// const list = document.querySelector("#list");
 
-
+// let files = ["fil1.js", "fil2.js", "..."];
 
 el.addEventListener("click", () => {
     list.classList.toggle("open");
