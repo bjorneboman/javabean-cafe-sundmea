e7b5173706e366f24f7e0532b7d70aaa399af40a
Isabelle Franzen <isabelle.amsalu@gmail.com>
2025-09-29 10:20:51 +0200

    add event listener to close menu on escape

--- isabelle-script.js
+++ isabelle-script.js
@@ -4,3 +4,9 @@ const menu = document.getElementById("list");
 burger.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
+
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    menu.classList.remove("open");
+  }
+});
