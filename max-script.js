6995d8ca257740717511f05f4dd14eb5c85eed49
Max Mattiasson <max@mattiasson.eu>
2025-09-29 11:12:54 +0200

    klar igen

--- max-script.js
+++ max-script.js
@@ -1,6 +1,7 @@
 const burger = document.getElementById("burger");
 const list = document.getElementById("list");
 
+// Toggles the menu to show or hide, displays button as X or burger
 burger.addEventListener("click", () => {
   list.classList.toggle("open");
   burger.classList.toggle("active");
